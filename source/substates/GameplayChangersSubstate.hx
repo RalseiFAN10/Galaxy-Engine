4121d6d6fef5c0ea6253164f2bffabc23a417f9b
RalseiFAN10 <ivantaikonotatsujin56@gmail.com>
2025-10-05 20:59:40 +0700

    Update GameplayChangersSubstate.hx

--- source/substates/GameplayChangersSubstate.hx
+++ source/substates/GameplayChangersSubstate.hx
@@ -26,20 +26,20 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 		if (goption.getValue() != "constant")
 		{
 			option.displayFormat = '%vX';
-			option.maxValue = 3;
+			option.maxValue = 1;
 		}
 		else
 		{
 			option.displayFormat = "%v";
-			option.maxValue = 6;
+			option.maxValue = 9;
 		}
 		optionsArray.push(option);
 
 		#if FLX_PITCH
 		var option:GameplayOption = new GameplayOption('Playback Rate', 'songspeed', 'float', 1);
-		option.scrollSpeed = 1;
-		option.minValue = 0.5;
-		option.maxValue = 3.0;
+		option.scrollSpeed = 0.7;
+		option.minValue = 0.1;
+		option.maxValue = 1.02;
 		option.changeValue = 0.05;
 		option.displayFormat = '%vX';
 		option.decimals = 2;
@@ -47,23 +47,23 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 		#end
 
 		var option:GameplayOption = new GameplayOption('Health Gain Multiplier', 'healthgain', 'float', 1);
-		option.scrollSpeed = 2.5;
+		option.scrollSpeed = 8.0;
 		option.minValue = 0;
-		option.maxValue = 5;
+		option.maxValue = 50;
 		option.changeValue = 0.1;
 		option.displayFormat = '%vX';
 		optionsArray.push(option);
 
 		var option:GameplayOption = new GameplayOption('Health Loss Multiplier', 'healthloss', 'float', 1);
-		option.scrollSpeed = 2.5;
-		option.minValue = 0.5;
-		option.maxValue = 5;
+		option.scrollSpeed = 8.0;
+		option.minValue = 0;
+		option.maxValue = 50;
 		option.changeValue = 0.1;
 		option.displayFormat = '%vX';
 		optionsArray.push(option);
 
-		optionsArray.push(new GameplayOption('Instakill on Miss', 'instakill', 'bool', false));
-		optionsArray.push(new GameplayOption('Practice Mode', 'practice', 'bool', false));
+		optionsArray.push(new GameplayOption('Instakill', 'instakill', 'bool', false));
+		optionsArray.push(new GameplayOption('Practice', 'practice', 'bool', false));
 		optionsArray.push(new GameplayOption('Botplay', 'botplay', 'bool', false));
 	}
 
